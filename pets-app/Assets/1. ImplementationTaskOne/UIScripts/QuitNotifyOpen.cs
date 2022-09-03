ecb586a9475966656492b69d6bfa60022858eadb
jane214 <jtan225@aucklanduni.ac.nz>
2022-09-04 07:13:46 +0800

    Implementation Task One
    1. Level Progress Bar
    2. Notification Panel
    3. Change Size Slider

---
+++ pets-app/Assets/1. ImplementationTaskOne/UIScripts/QuitNotifyOpen.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitNotifyOpen : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void closePanel()
+    {
+        Panel.SetActive(false);
+
+    }
+    public void openPanel()
+    {
+        Panel.SetActive(true);
+    }
+}
